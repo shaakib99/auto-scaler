5579e6c0e9c0e52ea4631e7e66553eafb27db303
Wahid Shakib <wsakib87@gmail.com>
2024-11-07 22:48:08 +0600

    exposed worker router in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from dotenv import load_dotenv
+from worker_service.route import router as worker_router
 
 async def lifespan(app):
     # load env
@@ -7,3 +8,7 @@ async def lifespan(app):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+routers: list[APIRouter] = [worker_router]
+for router in routers:
+    app.include_router(router)
