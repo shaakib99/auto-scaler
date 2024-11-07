22b3f87e5fbc154b01d10d0e4e9080ee6b763936
Wahid Shakib <wsakib87@gmail.com>
2024-11-07 22:22:06 +0600

    added environment variable schema

--- environment_variable_service/schema.py
+++ environment_variable_service/schema.py
@@ -1,18 +1,16 @@
 from database_service.mysql_service import MySQLDatabaseService
-from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, Enum, Boolean
-from common.enums import WorkerStatusEnum
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, Boolean
 from datetime import datetime
 
 Base = MySQLDatabaseService.get_instance().base
 
-class WorkerSchema(Base):
-    __tablename__ = "workers"
+class PortSchema(Base):
+    __tablename__ = "ports"
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
-    name = Column(String(255), nullable=False)
-    ram = Column(Integer, nullable=False, default=512)
-    cpu = Column(Integer, nullable=False, default=1)
-    status = Column(Enum(WorkerStatusEnum), default=WorkerStatusEnum.INIT)
+    worker_id = Column(Integer, ForeignKey('workers.id'), nullable=False)
+    key = Column(String(255), nullable=False)
+    value = Column(String(255), nullable=False)
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, nullable=False, default=datetime.now())
     updated_at = Column(DateTime, nullable=False, default=datetime.now())
---
+++ port_service/schema.py
@@ -0,0 +1,16 @@
+from database_service.mysql_service import MySQLDatabaseService
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, Boolean
+from datetime import datetime
+
+Base = MySQLDatabaseService.get_instance().base
+
+class PortSchema(Base):
+    __tablename__ = "ports"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    worker_id = Column(Integer, ForeignKey('workers.id'), nullable=False)
+    port_numnber = Column(Integer, nullable=False)
+    port_type = Column(String(255), nullable=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    updated_at = Column(DateTime, nullable=False, default=datetime.now())
