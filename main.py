75db6bf76cbb1d5b90f6bd8b1cbfff2a92db81ec
Wahid Shakib <wsakib87@gmail.com>
2024-11-08 09:56:36 +0600

    added port router in main file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, APIRouter
 from dotenv import load_dotenv
 from worker_service.route import router as worker_router
+from port_service.route import router as port_router
 
 async def lifespan(app):
     # load env
@@ -9,6 +10,6 @@ async def lifespan(app):
 
 app = FastAPI(lifespan=lifespan)
 
-routers: list[APIRouter] = [worker_router]
+routers: list[APIRouter] = [worker_router, port_router]
 for router in routers:
     app.include_router(router)
