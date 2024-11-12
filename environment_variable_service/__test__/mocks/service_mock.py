ec0760d41ba674af80b47b1372c7731d11824fe5
Wahid Shakib <wsakib87@gmail.com>
2024-11-12 21:19:27 +0600

    updated port and envrionment variable service

--- environment_variable_service/__test__/mocks/service_mock.py
+++ environment_variable_service/__test__/mocks/service_mock.py
@@ -6,6 +6,9 @@ from environment_variable_service.__test__.mocks.model_mock import *
 
 @pytest.fixture
 def mock_environment_variable_service(mock_database_service, mock_environment_variable_schema):
+    mock_database_service.create_one = AsyncMock(return_value = mock_environment_variable_schema)
+    mock_database_service.update_one = AsyncMock(side_effect = lambda id, data: mock_environment_variable_schema if id == 1 else None)
+    mock_database_service.get_one = AsyncMock(side_effect = lambda id, data: mock_environment_variable_schema if id == 1 else None)
+    mock_database_service.get_all = AsyncMock(return_value = [mock_environment_variable_schema])
     service = EnvironmentVariableService(environment_variable_model=mock_database_service)
-    service.create_one = AsyncMock(return_value = mock_environment_variable_schema)
     return service
--- port_service/__test__/mocks/service_mock.py
+++ port_service/__test__/mocks/service_mock.py
@@ -6,7 +6,11 @@ from port_service.__test__.mocks.model_mock import *
 
 @pytest.fixture
 def mock_port_service(mock_database_service, mock_port_schema):
+    mock_database_service.create_one = AsyncMock(return_value = mock_port_schema)
+    mock_database_service.update_one = AsyncMock(side_effect = lambda id, data: mock_port_schema if id == 1 else None)
+    mock_database_service.get_one = AsyncMock(side_effect = lambda id, data: mock_port_schema if id == 1 else None)
+    mock_database_service.get_all = AsyncMock(return_value = [mock_port_schema])
+
     service = PortService(port_model=mock_database_service)
-    service.create_one = AsyncMock(return_value = mock_port_schema)
     return service
 
