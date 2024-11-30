b5adbde42845dde07c702f11f118e0f1b3d868db
Wahid Shakib <wsakib87@gmail.com>
2024-11-30 18:12:58 +0600

    fixed test case issue for mock_docker_service

--- docker_service/__test__/mocks/service_mock.py
+++ docker_service/__test__/mocks/service_mock.py
@@ -1,9 +1,10 @@
 import pytest
-from unittest.mock import AsyncMock
-from docker_service.service import DockerContainerService
+from unittest.mock import AsyncMock, MagicMock
+from docker_service.service import DockerContainerService, docker
 
 @pytest.fixture
 def mock_docker_service():
+    docker.from_env = MagicMock()
     service = DockerContainerService()
     service.create_one = AsyncMock(return_value = "TEST")
     return service
