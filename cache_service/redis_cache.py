e93f237b5775d16c6285ad00625777bab59bcee1
Wahid Shakib <wsakib87@gmail.com>
2024-12-05 22:13:59 +0600

    added redis service

--- cache_service/redis_cache.py
+++ cache_service/redis_cache.py
@@ -6,11 +6,11 @@ class RedisCache(CacheABC):
     def __init__(self):
         self.redis = Redis(host=os.getenv('REDIS_HOST', 'localhost'), port=os.getenv('REDIS_PORT', 6379), db=os.getenv('REDIS_DB', 'cache_db'))
 
-    async def get(self, key):
+    async def get(self, key: str):
         return self.redis.hget(key)
 
-    async def set(self, key, value, expire=None):
-        self.redis.hset(key, value)
+    async def set(self, key: str, value: dict, expire=None):
+        self.redis.hset(key, value.__str__())
     
     async def delete(self, key):
         return self.redis.hdel(key)
